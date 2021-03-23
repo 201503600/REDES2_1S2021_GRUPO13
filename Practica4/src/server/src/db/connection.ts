e7cc139595530062576e0c2b5ffa99eefd0b12ef
sandymerida <sandymeridah@gmail.com>
2021-03-23 15:04:38 -0600

    add: database

--- Practica4/src/server/src/db/connection.ts
+++ Practica4/src/server/src/db/connection.ts
@@ -1,14 +1,15 @@
-import mongoose from "mongoose";
+import morgan from 'morgan';
+import  mongoose  from "mongoose";
 
-let mongoDB = "mongodb://mongo:27017/redestemp";
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(mongoDB,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+let mongoDB = "mongodb://mongo-container:27017/redes2";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, " ----  MongoDB connection error   ---- "));
+morgan(':method :url :status :res[content-length] - :response-time ms')
 
-mongoose.connection.on("open", () => console.log("Succesfully mongodb"));
