db403b73a87913a61cfe2416ce6c4b5611173ad7
sandymerida <sandymeridah@gmail.com>
2021-03-26 11:24:56 -0600

    update: static

--- Practica4/src/frontend/pr4front/src/components/newReport.jsx
+++ Practica4/src/frontend/pr4front/src/components/newReport.jsx
@@ -23,7 +23,7 @@ export default function NewReport(){
             curso: curso,
             detalle: detalle
         }
-        let response = await axiosInstance.post(process.env.BACK||"http://172.35.73.40:3001/", objeto);
+        let response = await axiosInstance.post(process.env.BACK||"http://3.13.15.158:3001/", objeto);
         console.log(response.data);
         setCarnet("");
         setNombre("");
